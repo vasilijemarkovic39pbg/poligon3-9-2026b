d0b8580908ae0c4229beb1499943887f6e3c114f
Ucenik <Ucenik@DESKTOP-A6MV7AL>
2026-03-23 19:45:50 +0100

    radi nadam se

--- Poligon.cs
+++ Poligon.cs
@@ -53,7 +53,16 @@ namespace poligon3_9_2026b
         }
         public static Poligon ucitaj()
         {
-            return null;
+            StreamReader ulaz = new StreamReader("poligon.txt");
+            int n = Convert.ToInt32(ulaz.ReadLine());
+            Poligon novi = new Poligon(n);
+            for (int i = 0; i < n; i++)
+            {
+                novi.teme[i] = new Tacka();
+                novi.teme[i].x = Convert.ToDouble(ulaz.ReadLine());
+                novi.teme[i].y = Convert.ToDouble(ulaz.ReadLine());
+            }
+            return novi;
         }
     }
 }
